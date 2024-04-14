34f2e118d259f6671f3812b7619a46faa30a511e
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-04-14 19:53:42 +0700

    Version 1.3

--- V102/Class_50/CM/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class RealTimeDataSerializer(serializers.Serializer):
-    dayofweek = serializers.IntegerField()
-    time = serializers.TimeField()
--- V102/Class_50/app_User/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from SM.models import Mark
-
-class MarkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mark
-        fields = ['subject', 'scores']
